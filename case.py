a934629c849c099458c0a201ade0d7112c495571
Omar Marnissi <omarmarnissi777@gmail.com>
2025-03-14 10:23:17 +0100

    resolving merge squash conflict

--- case.py
+++ case.py
@@ -1,4 +1,13 @@
 Data=[1,3,5]
+<<<<<<< HEAD
 Som=sum(Data)
 print('la somme est: ', som)
 
+=======
+if Data:
+	print('la somme est: ', sum(Data))
+	print('le min est: ', min(Data))
+	print('le max est: ', max(Data))
+else:
+	print('dossier vide')
+>>>>>>> casee2
